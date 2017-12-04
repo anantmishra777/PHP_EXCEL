1f8a524f6f4b1bc51a5bf0de85c28b8535f75db1
Anant Mishra <anant@novostack.com>
2017-12-04 18:35:52 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
 	try
 	{
-		$conn = new PDO("mysql:host=localhost; dbname=test_db; charset=utf8", "root", "");
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$conn = new PDO("mysql:host=localhost; dbname=test_db; charset=utf8", "root", "");  //change values accordingly
+    		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(PDOException $e)
-    {
-        echo "-------------".$e->getMessage()."-------------";
-    }
-
-    define('EOL', (PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+	{
+		echo "-------------".$e->getMessage()."-------------";
+	}
+    	define('EOL', (PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
 ?>
